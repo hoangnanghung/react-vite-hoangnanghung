772dba336afe731b6643ffb54d877043d52c9df3
hoang nang hung <hoanghung10041997@gmail.com>
2026-04-13 14:49:28 +0700

    video 36

--- src/assets/components/todo/ToDoNew.jsx
+++ src/assets/components/todo/ToDoNew.jsx
@@ -2,12 +2,34 @@
 const ToDoNew = (props) => {
     const { addNewToDo } = props;
 
+    const handleClick = () => {
+        alert("handleClick");
+    }
+
+    const handleOnChange = (name) => {
+        console.log(name);
+    }
+    
+    /* cách code 1 */ 
+    // return (
+    //     <div className="todo-form">
+
+            
+    //         <input onChange={handleOnChange} type="text" placeholder="Enter......" />
+    //         <button onClick={handleClick}>Add</button>
+    //     </div>
+    // )
+
+    /* cách code 2 */
+
     return (
         <div className="todo-form">
-            <input type="text" placeholder="Enter your task" />
-            <button onClick={addNewToDo}>Add</button>
+            <input onChange={(event) => handleOnChange(event.target.value)} type="text" placeholder="Enter......" />
+            <button onClick={handleClick}>Add</button>
         </div>
     )
+
+
 }
 
 export default ToDoNew
