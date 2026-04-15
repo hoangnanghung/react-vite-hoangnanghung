278678e18551d03eb895f4dac7ce483038060a46
hoang nang hung <hoanghung10041997@gmail.com>
2026-04-15 08:44:59 +0700

    video 67 State Form

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,38 +1,73 @@
 
-import React from 'react';
+
+import React, { useState } from 'react';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Button, Input, Space } from 'antd';
 
 const UserForm = () => {
     const [passwordVisible, setPasswordVisible] = React.useState(false);
+    const [fullname, setFullname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    // console.log(fullname);
+
+    const handleClickBtn= () => {
+        // console.log(fullname);
+        // console.log(email);
+        // console.log(password);
+        // console.log(phone);
+    }
     return(
         <div className='user-form' style={{margin: "20px 0px", padding: "20px"}}>
             <div style={{display: "flex", gap: "20px", flexDirection: "column"}}>
                     <div>   
                         <span>Full name</span>
-                        <Input placeholder="Full name" />
+                        <Input placeholder="Full name" 
+                        value={fullname} 
+                        onChange={(event) => {
+                            {
+                                setFullname(event.target.value);
+                            }
+                        }}/>
                     </div>
                     
                     <div>   
                         <span>Email</span>
-                        <Input placeholder="Email" />
+                        <Input placeholder="Email"
+                        value={email} 
+                        onChange={(event) => {
+                            {
+                                setEmail(event.target.value);
+                            }
+                        }}
+                         />
                     </div>
                     <div>   
                         <span>Password</span>
                         <Input.Password
                             placeholder="input password"
+                            value={password}
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                            }}
                             iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
                         />
-                                
                     </div>
                     <div>   
                         <span>Phone number</span>
-                        <Input placeholder="Phone number" />
+                        <Input placeholder="Phone number"
+                        value={phone} 
+                        onChange={(event) => {
+                            {
+                                setPhone(event.target.value);
+                            }
+                        }} />
                     </div> 
 
                     <div>   
                         
-                        <Button type="primary">Create User</Button>
+                        <Button onClick={() => handleClickBtn()} type="primary">Create User</Button>
                     </div>    
             </div>
             
